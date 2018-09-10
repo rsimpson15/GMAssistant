fe6f47b59202f3d4f4ac2aff53c6408252b6bd82
Devon Smith <dsmith16@mail.wou.edu>
2018-09-10 03:57:05 -0700

    Created test project for character sheet to Json

---
+++ test_projects/character_sheet_demo/character_sheet_demo/Character.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace character_sheet_demo
+{
+    /// <summary>
+    /// A Generic Character Class that can be extended to include any set of
+    /// attributes for a character from a variety of systems.
+    /// </summary>
+    [Serializable]
+    class Character
+    {
+        /// <summary>
+        /// Auto properties for this object.
+        /// </summary>
+        // Profile
+        public Dictionary<string, string> Profile;
+        // Stats
+        public Dictionary<string, int> Stats;
+        // Skills
+        public Dictionary<string, Dictionary<string, int>> Skills;
+        // Traits
+        public  List<Dictionary<string, string>> Traits;
+        // Other Traits
+        public Dictionary<string, int> Other;
+    }
+}
